a3c8a8652520f40d47a04cd5d51f088e0fb7b995
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-05-17 22:59:16 +0200

    Added connection between The multiworld world, and the world below it

--- src/me/ferrybig/bukkit/plugins/multiworld/rev2/WorldDefinition.java
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/WorldDefinition.java
@@ -1,5 +1,8 @@
 package me.ferrybig.bukkit.plugins.multiworld.rev2;
 
+import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.NativeWorld;
+
 public interface WorldDefinition {
 
+        public NativeWorld getWorld();
 }
---
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/natives/NativeWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.bukkit.plugins.multiworld.rev2.natives;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface NativeWorld {
+    public UUID getID();
+    
+    public String getName();
+    
+    public int getDimension();
+            
+    public Object getNative();
+}
