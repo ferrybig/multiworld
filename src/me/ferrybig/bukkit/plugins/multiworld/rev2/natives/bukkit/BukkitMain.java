4b119bc78a2a6046c12d0c4ce34cdef4630b9818
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-05-23 21:25:15 +0000

    Fixed compiler error

--- src/me/ferrybig/bukkit/plugins/multiworld/rev2/natives/bukkit/BukkitMain.java
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/natives/bukkit/BukkitMain.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.Native;
+import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.NativeConsoleCommandSender;
 import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.NativeGenerator;
 import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.NativePermissionsHolder;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,6 +28,11 @@ public class BukkitMain extends JavaPlugin implements Native {
 
     @Override
     public Collection<? extends NativePermissionsHolder> getOps() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public NativeConsoleCommandSender getConsoleCommandSender() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
