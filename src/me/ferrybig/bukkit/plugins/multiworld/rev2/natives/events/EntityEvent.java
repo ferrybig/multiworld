a646f58ecfe3fadb7d961ebb25eb22a36ac7b893
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-05-27 22:30:49 +0200

    Added events multiworld is going to use

---
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/natives/events/EntityEvent.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.bukkit.plugins.multiworld.rev2.natives.events;
+
+import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.entities.NativeEntity;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface EntityEvent extends Event {
+    public NativeEntity getEntity();
+}
