9cc33956ed285de32387fa69b2201dad1c8afbcf
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-06-11 22:57:21 +0200

    Added nativeTaskSchedular

---
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/natives/NativeTaskManager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.bukkit.plugins.multiworld.rev2.natives;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface NativeTaskManager {
+    public void scheduleTask(NativeTask task);
+
+    public static interface NativeTaskDefinition {
+
+        public void cancel();
+    }
+
+    public static interface NativeTask {
+
+        public void run(NativeTaskDefinition def);
+    }
+}
