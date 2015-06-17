0bc840af32f446e33c98dec143805f70ea7b466d
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-06-17 23:04:17 +0200

    Added multiworld based chunk generator

---
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/chunkgeneration/MultiWorldChunkGenerator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.bukkit.plugins.multiworld.rev2.chunkgeneration;
+
+import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.NativeWorld;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface MultiWorldChunkGenerator {
+    
+    public short[][] createChunk(int x, int z, NativeWorld world);
+    
+}
