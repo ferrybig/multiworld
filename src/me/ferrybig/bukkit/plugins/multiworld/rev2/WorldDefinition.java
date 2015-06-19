4456fa60338624b2c3c7b9d866fa824a3721d875
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-06-19 20:28:07 +0000

    added WorldDefinition.getEnvironment

--- src/me/ferrybig/bukkit/plugins/multiworld/rev2/WorldDefinition.java
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/WorldDefinition.java
@@ -1,8 +1,15 @@
 package me.ferrybig.bukkit.plugins.multiworld.rev2;
 
+import java.util.UUID;
 import me.ferrybig.bukkit.plugins.multiworld.rev2.natives.NativeWorld;
 
 public interface WorldDefinition {
 
         public NativeWorld getWorld();
+        
+        public String getName();
+        
+        public UUID getUUID();
+        
+        
 }
