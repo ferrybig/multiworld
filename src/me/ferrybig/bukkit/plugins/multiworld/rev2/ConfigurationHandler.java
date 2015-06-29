c6518b57b56a99174bba2f8c2f582ea77900a5bc
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-06-29 23:19:13 +0200

    Added configuration handler

---
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/ConfigurationHandler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.bukkit.plugins.multiworld.rev2;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface ConfigurationHandler {
+    public void saveConfig();
+    
+    public void reloadConfig();
+    
+    public boolean isConfigUpdatedWithoutNotice();
+    
+    public void saveWorld(WorldDefinition world);
+    
+}
