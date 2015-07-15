d10a11857f85001700468bd0ef37e5018f24e387
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-07-15 20:10:25 +0000

    Added NativePluginProperties

---
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/natives/NativePluginProperties.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.bukkit.plugins.multiworld.rev2.natives;
+
+import java.net.URL;
+import java.util.Collection;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface NativePluginProperties {
+
+    public Collection<String> getCreators();
+
+    public URL getOrigin();
+
+}
