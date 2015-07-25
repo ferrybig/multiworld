f32892b9f8700da2a9bfa4d4b17c5a6ce0ecc66e
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-07-25 12:19:02 +0200

    Added NativeMaterials and related subclasses

---
+++ src/me/ferrybig/bukkit/plugins/multiworld/rev2/natives/materials/NativeMaterials.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.bukkit.plugins.multiworld.rev2.natives.materials;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface NativeMaterials {
+    public NativeMaterial requireMaterial(String fullname);
+    
+    public NativeMaterial requireMaterial(short id);
+    
+    public NativeMaterial getMaterial(String fullname);
+    
+    public NativeMaterial getMaterial(short id);
+    
+    public NativeMaterial[] getClosestMatches(String name, int distance);
+    
+    public boolean hasDynamicItems();
+    
+    public boolean canRegisterItems();
+    
+    
+}
